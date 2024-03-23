cf58c8bcbde7ed738299d998ffec2cbaef217dc0
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2024-03-23 15:33:35 -0500

    Navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import '../style/NavBar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>Portfolio</li>
+        <li>Contact Info</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
