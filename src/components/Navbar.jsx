de8fa7816036e0e604283090c2ce07a6e8dcd4cd
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2024-03-24 21:26:39 -0500

    more navbar stuff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,15 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Navbar from '/src/style/NavBar.css';
 
 function BasicNav() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">Joey Porter</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <navbar className="navbar">
+      <h1 className="name">Joey Porter's Portfolio</h1>
+      <ul>
+        <li><a href="#about">About Me</a></li>
+        <li><a href="#mywork">My Work</a></li>
+        <li><a href="#contact">Contact Me</a></li>
+      </ul>
+    </navbar>
   );
 }
 
