353b584c9ab68189d96a64191a00cc247a6e638a
Joey Porter <148152563+Joey2522@users.noreply.github.com>
2024-04-03 18:17:47 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import '../styles/Navbar.css';
-import mainPic from '/src/images/profilePic.png';
+import mainPic from '/src/images/Porter-0111.jpg';
 
 function Navbar () {
  return (
@@ -15,6 +15,8 @@ function Navbar () {
       </ul>
     </nav>
     <Link to={''}><img src={ mainPic } alt="Handsom Man" class="picture"/></Link>
+    <h1 className="webDev">Web Developer</h1>
+    <h2 className="list">HTML - CSS - JAVASCRIPT - REACT - NODE</h2>
     <header className="name">Joey Porter</header>
   </>
  );
